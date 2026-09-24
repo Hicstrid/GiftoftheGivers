a951386ee170a1db6751fb00e8cdb9098e0edb36
lesedindivhuho <nemaunganilesedi@gmail.com>
2026-09-25 01:34:19 +0200

    fix: progeam cs not wanting to stat the server and added local file to gitignore

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Part1.Data;
 using System.IO;
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,8 @@ if (!Directory.Exists(expectedWebRoot))
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages(); //enable Razor pages in addition to MVC controllers/views
+builder.Services.AddDbContext<Part1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Part1Context")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,8 +22,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-else {
-    app.UseExceptionHandler("?/Home/Error");
+else
+{
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -38,7 +42,3 @@ app.MapControllerRoute(
 
 app.Run();
 
-builder.Services.AddDbContext<Part1Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Part1Context")));
-
-builder.Services.AddControllersWithViews();
